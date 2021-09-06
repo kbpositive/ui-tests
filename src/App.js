546fd2a00b728bca18bcd0a7c3d6436b5f3142ac
Kadeem Baldwin <Kadbaldwin@gmail.com>
2021-09-06 15:20:30 -0500

    added typescript button components/ fixed routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Header from './components/layout/Header';
+import Button from "./components/layout/CustomButtonComponent";
 import './App.css';
 
-class App extends Component {
-  state = {
-    todos: []
-  }
 
-  render() {
-    return (
+function App() {
+  return (
+    <>
       <Router>
-        <div className="App">
-          <div className="container">
-            <Header />
-          </div>
-        </div>
-      </Router>
-    );
-  }
+        <Header />
+        <h1>Colorful Custom Button Components</h1>
+        <Button
+          border="none"
+          color="pink"
+          height="200px"
+          onClick={() => alert("You clicked on the pink circle!")}
+          radius="50%"
+          width="200px"
+          children="I'm a pink circle!"
+        />
+        <br></br>
+        <br></br>
+
+        <Button
+          border="dotted"
+          color="#f5bc42"
+          height="200px"
+          onClick={() => console.log("You clicked on the orange circle!")}
+          radius="50%"
+          width="200px"
+          children="I'm an orange circle!"
+        />
+        <br></br>
+        <br></br>
+        <Button
+          border="dashed"
+          color="#fdffc4"
+          height="200px"
+          onClick={() => console.log("You clicked on the yellow square!")}
+          radius="10%"
+          width="200px"
+          children="I'm a yellow square!"
+        /></Router>
+    </>
+  );
 }
 
 export default App;
