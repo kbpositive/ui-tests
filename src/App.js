96855b3f415c22a3d92adb0516e388fded3a3309
Kadeem Baldwin <Kadbaldwin@gmail.com>
2021-09-06 16:23:08 -0500

    added test button

--- src/App.js
+++ src/App.js
@@ -6,43 +6,28 @@ import './App.css';
 
 
 function App() {
+  const testBg = {}
+  const testButton = {
+    width: "15rem",
+    height: "4rem",
+    borderRadius: "1rem",
+    justifySelf: "center",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    cursor: "pointer",
+    transition: ".3s ease",
+    boxShadow: ".3rem .3rem .6rem #c8d0e7, -.2rem -.2rem .5rem #FFFFFF"
+  };
   return (
     <>
       <Router>
         <Header />
-        <h1>Colorful Custom Button Components</h1>
-        <Button
-          border="none"
-          color="pink"
-          height="200px"
-          onClick={() => alert("You clicked on the pink circle!")}
-          radius="50%"
-          width="200px"
-          children="I'm a pink circle!"
-        />
         <br></br>
-        <br></br>
-
-        <Button
-          border="dotted"
-          color="#f5bc42"
-          height="200px"
-          onClick={() => console.log("You clicked on the orange circle!")}
-          radius="50%"
-          width="200px"
-          children="I'm an orange circle!"
-        />
-        <br></br>
-        <br></br>
-        <Button
-          border="dashed"
-          color="#fdffc4"
-          height="200px"
-          onClick={() => console.log("You clicked on the yellow square!")}
-          radius="10%"
-          width="200px"
-          children="I'm a yellow square!"
-        /></Router>
+        <div style={testBg}>
+          <div style={testButton}>button</div>
+        </div>
+      </Router>
     </>
   );
 }
