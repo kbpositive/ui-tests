765b13c79a62877d72fafefe36156221713ca1dc
Kadeem Baldwin <Kadbaldwin@gmail.com>
2021-09-08 15:37:24 -0500

    refactored header to tailwind

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -3,24 +3,14 @@ import { Link } from 'react-router-dom';
 
 function Header() {
     return (
-        <header style={headerStyle}>
-            <h1>Components</h1>
+        <header class="flex font-sans w-auto h-1/4 rounded-3xl my-5 mx-3 p-3 justify-center" style={headerStyle}>
+            <h1 class="text-xl" >Components</h1>
         </header>
     )
 }
 
 const headerStyle = {
-    width: "auto",
-    height: "10%",
     color: "#4E67B1",
-    fontFamily: "Arial",
-    marginTop: "20px",
-    borderRadius: "1rem",
-    justifySelf: "center",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-    transition: ".3s ease",
     boxShadow: ".3rem .3rem .6rem #212C4C, -.2rem -.2rem .5rem #4E67B1",
 }
 
