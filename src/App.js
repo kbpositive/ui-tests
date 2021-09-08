6be6558bcb299acc42e8fe5d9d3e8c601194a6a0
Kadeem Baldwin <Kadbaldwin@gmail.com>
2021-09-08 12:58:50 -0500

    fixed overlapping buttons

--- src/App.js
+++ src/App.js
@@ -11,7 +11,8 @@ function App() {
     background: '#2F3E6C',
     position: "relative",
   }
-  const testButton = {
+
+  const t1 = {
     width: "9rem",
     height: "3rem",
     margin: "0% 15%",
@@ -29,15 +30,35 @@ function App() {
     transition: ".3s ease",
     boxShadow: ".3rem .3rem .6rem #212C4C, -.2rem -.2rem .5rem #4E67B1",
   };
+
+  const t2 = {
+    width: "9rem",
+    height: "3rem",
+    margin: "0% 15%",
+    color: "#4E67B1",
+    fontFamily: "Arial",
+    position: "absolute",
+    top: "28%",
+    marginTop: "-30px",
+    borderRadius: "1rem",
+    justifySelf: "center",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    cursor: "pointer",
+    transition: ".3s ease",
+    boxShadow: ".3rem .3rem .6rem #212C4C, -.2rem -.2rem .5rem #4E67B1",
+  };
+
   return (
     <>
       <Router>
         <div style={testBg}>
           <Header />
           <br></br>
-          <div style={testButton}>button</div>
+          <div style={t1}>button 1</div>
           <br></br>
-          <div style={testButton}>button</div>
+          <div style={t2}>button 2</div>
         </div>
       </Router>
     </>
